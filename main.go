3db7be27d218dad9bf16d9be4967b19f63879716
MalgAmoe <malgamoe@protonmail.com>
2025-11-06 15:11:46 +0100

    add borders

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"time"
 
@@ -46,6 +47,10 @@ type GameState struct {
 
 	// player
 	player position
+
+	// borders
+	topB    [MIN_WIDTH]int
+	bottomB [MIN_WIDTH]int
 }
 
 func main() {
@@ -60,8 +65,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	borderStyle := tcell.StyleDefault.Background(tcell.ColorBlack)
-	gameAreaStyle := tcell.StyleDefault.Background(tcell.ColorDarkBlue)
+	borderStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorPink)
+	gameAreaStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorBlueViolet)
 	s.SetStyle(borderStyle)
 
 	gameState := GameState{
@@ -73,12 +78,17 @@ func main() {
 		player:        position{10, MIN_HEIGTH / 2},
 	}
 
+	for i := range gameState.bottomB {
+		gameState.topB[i] = 2
+		gameState.bottomB[i] = MIN_HEIGTH - 2
+	}
+
 	// key events
 	inputCh := make(chan tcell.Key, 10)
 	go handleInput(gameState.s, inputCh)
 
 	// update frame based on ticker
-	ticker := time.NewTicker(33 * time.Millisecond)
+	ticker := time.NewTicker(66 * time.Millisecond)
 	defer ticker.Stop()
 
 	// game loop
@@ -90,6 +100,7 @@ func main() {
 	}
 
 	gameState.s.Fini()
+	os.Exit(0)
 }
 
 func handleInput(s tcell.Screen, inputCh chan tcell.Key) {
@@ -107,7 +118,7 @@ func handleInput(s tcell.Screen, inputCh chan tcell.Key) {
 }
 
 func update(g *GameState, inputCh <-chan tcell.Key) {
-
+	// player position
 outer:
 	for {
 		select {
@@ -140,6 +151,9 @@ outer:
 	} else if g.player.x >= g.minWidth-1 {
 		g.player.x = g.minWidth - 1
 	}
+
+	// borders
+	updateAllBorders(&g.bottomB, &g.topB)
 }
 
 func draw(g *GameState) {
@@ -156,17 +170,23 @@ func draw(g *GameState) {
 	// draw game screen
 	for y := playAreaY; y < playAreaY+g.minHeight; y++ {
 		for x := playAreaX; x < playAreaX+g.minWidth; x++ {
-			g.s.SetContent(x, y, ' ', nil, g.gameAreaStyle)
+			// draw land if other part of the border
+			if (y-playAreaY) > g.bottomB[x-playAreaX] ||
+				(y-playAreaY) <= g.topB[x-playAreaX] {
+				g.s.SetContent(x, y, '#', nil, g.gameAreaStyle)
+			} else {
+				g.s.SetContent(x, y, ' ', nil, g.gameAreaStyle)
+			}
 		}
 	}
 
 	// player
-	g.s.SetContent(playerX, playerY, '>', nil, g.gameAreaStyle.Foreground(tcell.ColorYellow))
+	g.s.SetContent(playerX, playerY, '>', nil, g.gameAreaStyle.Foreground(tcell.ColorPink))
 
 	// terminal size check
 	if termWidth < g.minWidth || termHeight < g.minHeight {
 		text := fmt.Sprintf("Terminal too small! Need at least 60x30, now is %dx%d", termWidth, termHeight)
-		drawText(g.s, 1, 1, g.borderStyle, text)
+		drawText(g.s, (termWidth-len(text))/2, 0, g.borderStyle, text)
 	}
 
 	// info
@@ -179,4 +199,53 @@ func drawText(s tcell.Screen, x, y int, style tcell.Style, text string) {
 	for i, r := range text {
 		s.SetContent(x+i, y, r, nil, style)
 	}
+}
+
+func updateAllBorders(bB *[MIN_WIDTH]int, tB *[MIN_WIDTH]int) {
+	choice1 := rand.Float32()
+	direction1 := 0
+
+	choice2 := rand.Float32()
+	direction2 := 0
+
+	// check top border range
+	if choice1 < 0.4 && (tB[MIN_WIDTH-1]) < MIN_HEIGTH-3 {
+		direction1 = -1
+	} else if choice1 > 0.6 && tB[MIN_WIDTH-1] > 3 {
+		direction1 = 1
+	}
+
+	// check bottom border range
+	if choice2 < 0.3 && bB[MIN_WIDTH-1] < MIN_HEIGTH-3 {
+		direction2 = -1
+	} else if choice2 > 0.6 && (bB[MIN_WIDTH-1]) > 3 {
+		direction2 = 1
+	}
+
+	// avoid borders overlap
+	if tB[MIN_WIDTH-1]+5 >= (bB[MIN_WIDTH-1]) {
+		direction1 = 1
+		direction2 = -1
+	}
+
+	updateBorder(tB, direction1)
+	updateBorder(bB, direction2)
+}
+
+func updateBorder(b *[MIN_WIDTH]int, direction int) {
+	var newBorder int
+	for i := range MIN_WIDTH - 1 {
+		b[i] = b[i+1]
+	}
+
+	switch direction {
+	case 0:
+		newBorder = b[MIN_WIDTH-2]
+	case 1:
+		newBorder = b[MIN_WIDTH-2] - 1
+	case -1:
+		newBorder = b[MIN_WIDTH-2] + 1
+	}
+
+	b[MIN_WIDTH-1] = newBorder
 }
